8f9058ebb93222d30e59b4e5e7b44ac95b952e4f
Musfiq <rahmanmdmusfiqur3@gmail.com>
2026-06-23 02:03:04 +0600

    Lab 2: add product route in program.cs (after completing the prelab task)

---
+++ Lab02/F1stMVCApp/F1stMVCApp/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace F1stMVCApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        //displays the student list
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Student List";
+            ViewBag.Message = "Welcome to the student module";
+            return View();
+        }
+        //displays details for a specific student
+        public IActionResult Details(int id)
+        {
+            ViewBag.StudentId = id;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return Content("This is the student module");
+        }
+    }
+}
