2c577f8274f0c59f138574101fe77b553cb88c4a
Musfiq <rahmanmdmusfiqur3@gmail.com>
2026-06-23 01:48:57 +0600

    basic 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////int[] marks = new int[5];
+            ////marks[1] = 75;
+            ////Console.WriteLine(marks[1]);
+
+            ////List<int> marks = new List<int>();
+            ////marks.Add(100);
+            ////marks.Add(79);
+            ////marks.Add(87);
+
+            ////marks.RemoveAt(index: 1);
+
+            //List<int> numbers = new List<int>
+            //{
+            //    12, 5, 8, 21, 15, 3, 18, 25, 7, 30,
+            //    14, 9, 22, 11, 6, 17, 28, 4, 13, 20
+            //};
+            //int sum = numbers.Sum();
+            //int max = numbers.Max();
+            //int min = numbers.Min();
+            //double avg = numbers.Average();
+            //var max1 = numbers.OrderByDescending(x => x).First();
+            //var second = numbers.Where(e => e < max).OrderByDescending(x=>x).First();
+            //Console.WriteLine(second);
+            //List<int> even = numbers.Where(x => x % 2 == 0).ToList();
+            //Console.WriteLine();
+            //foreach (var n in even)
+            //{
+            //    Console.WriteLine(n);
+            //}
+            //int evensum = even.Sum();
+            //Console.WriteLine(evensum);
+
+            //var secondhigh = numbers.Distinct().OrderByDescending(e => e).Skip(1).First();
+            //Console.WriteLine(secondhigh);
+            //var evensum2 = numbers. Where (e =>e % 2 == 0).Sum();
+            //Console.WriteLine(evensum2);
+
+
+            ////List<int> greater15 = numbers.Where(x => x > 15).ToList();
+
+            ////List<int> greaterthanavg = numbers.Where(x => x>avg).ToList();
+
+
+
+
+            ////foreach (var n in greater15)
+            ////{
+            ////    Console.WriteLine(n);
+            ////}
+
+            ////Console.WriteLine(avg);
+            ////foreach (var n in greaterthanavg)
+            ////{
+            ////    Console.WriteLine(n);
+            ////}
+            ////Console.WriteLine();
+
+
+
+
+            //////for (int i = 0; i < numbers.Count; i++)
+            //////{
+            //////    if (numbers[i] > 15)
+            //////    {
+            //////        greater15.Add(i);
+            //////    }
+            //////}
+
+            //////Console.WriteLine(sum);
+            //////Console.WriteLine(max);
+            //////Console.WriteLine(min);
+            //////Console.WriteLine(avg);
+            //////Console.WriteLine(greater15);
+            ///
+
+
+//            List<string> names = new List<string>
+//{
+//    "Jahid",
+//    "Hasan",
+//    "Rahim",
+//    "Karim",
+//    "Sakib",
+//    "Tamim",
+//    "Mushfiq",
+//    "Mahmudullah",
+//    "Nayeem",
+//    "Rakib",
+//    "Jamil",
+//    "Karim",
+//    "Sakib"
+//};
+
+//            List<string> startwithA = names.Where(x=> x.StartsWith("J")).ToList();
+
+//            foreach (string name in startwithA)
+//            {
+//                Console.WriteLine(name);
+//            }
+
+//            var bigName = names.OrderByDescending(x => x.Length).First();
+//            Console.WriteLine(bigName);
+
+
+        }
+    }
+}
