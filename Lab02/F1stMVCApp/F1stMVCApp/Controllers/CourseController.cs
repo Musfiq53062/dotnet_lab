529fa570fbfa359a4d7c7722780f7ffa8390a441
Musfiq <rahmanmdmusfiqur3@gmail.com>
2026-06-23 02:03:44 +0600

    add product route

---
+++ Lab02/F1stMVCApp/F1stMVCApp/Controllers/CourseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace F1stMVCApp.Controllers
+{
+    [Route("courses")]
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
